eac59dd05e3fd20fe4ecda00b53e3a5c9be61696
Jean-Claude AZIAHA <aziaha.formations@gmail.com>
2022-05-03 20:25:28 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    /**
+     * -----------------------------------------------
+     * Bienvenue dans notre framework
+     * 
+     * Ce fichier représente le FrontController
+     * C'est donc le premier controller a être chargé
+     * par l'application
+     * -----------------------------------------------
+    */
+
+
+    /*
+     * Chargement du Kernel 
+     */
+    
+
+
+
+
